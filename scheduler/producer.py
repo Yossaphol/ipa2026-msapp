2acf0f2f6cb9fc4fed764e9af9c8a3b73e54fb03
Yossaphol Manovorakul <yossaphol3502@gmail.com>
2026-09-06 19:07:34 +0700

    done part9

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -1,16 +1,45 @@
+import os
 import pika
 
+
 def produce(host, body):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+    username = os.environ.get("RABBITMQ_USER", "admin")
+    password = os.environ.get("RABBITMQ_PASSWORD", "rabbitmq")
+
+    credentials = pika.PlainCredentials(username, password)
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=host,
+            credentials=credentials
+        )
+    )
+
     channel = connection.channel()
 
-    channel.exchange_declare(exchange="jobs", exchange_type="direct")
-    channel.queue_declare(queue="router_jobs")
-    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+    channel.exchange_declare(
+        exchange="jobs",
+        exchange_type="direct"
+    )
 
-    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+    channel.queue_declare(
+        queue="router_jobs"
+    )
+
+    channel.queue_bind(
+        queue="router_jobs",
+        exchange="jobs",
+        routing_key="check_interfaces"
+    )
+
+    channel.basic_publish(
+        exchange="jobs",
+        routing_key="check_interfaces",
+        body=body
+    )
 
     connection.close()
+
 
 if __name__ == "__main__":
     produce("localhost", "192.168.1.44")
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,4 +1,5 @@
-import time, pika
+import os
+import time
 
 from bson import json_util
 from producer import produce
@@ -11,23 +12,46 @@ def scheduler():
     next_run = time.monotonic()
     count = 0
 
+    rabbitmq_host = os.environ.get("RABBITMQ_HOST", "rabbitmq")
+
     while True:
         now = time.time()
-        now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
-        ms = int((now % 1) * 1000)  
+        now_str = time.strftime(
+            "%Y-%m-%d %H:%M:%S",
+            time.localtime(now)
+        )
+
+        ms = int((now % 1) * 1000)
         now_str_with_ms = f"{now_str}.{ms:03d}"
-        print(f"[{now_str_with_ms}] run #{count}")
+
+        print(
+            f"[{now_str_with_ms}] run #{count}",
+            flush=True
+        )
 
         try:
             for data in get_router_info():
                 body_bytes = json_util.dumps(data).encode("utf-8")
-                produce("localhost", body_bytes)
+
+                produce(
+                    rabbitmq_host,
+                    body_bytes
+                )
+
         except Exception as e:
-            print(e)
+            print(e, flush=True)
             time.sleep(3)
+
         count += 1
         next_run += INTERVAL
-        time.sleep(max(0.0, next_run - time.monotonic()))
 
-if __name__=='__main__':
+        time.sleep(
+            max(
+                0.0,
+                next_run - time.monotonic()
+            )
+        )
+
+
+if __name__ == "__main__":
     scheduler()
