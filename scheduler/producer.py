c616c82b21f65ab5c7aea909b94cedc08ef1c82a
Yossaphol Manovorakul <yossaphol3502@gmail.com>
2026-09-15 11:00:38 +0700

    fix flake8

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -1,6 +1,7 @@
 import os
 import pika
 
+
 def produce(host, body):
     username = os.environ.get("RABBITMQ_USER", "admin")
     password = os.environ.get("RABBITMQ_PASSWORD", "rabbitmq")
@@ -8,34 +9,20 @@ def produce(host, body):
     credentials = pika.PlainCredentials(username, password)
 
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=host,
-            credentials=credentials
-        )
+        pika.ConnectionParameters(host=host, credentials=credentials)
     )
 
     channel = connection.channel()
 
-    channel.exchange_declare(
-        exchange="jobs",
-        exchange_type="direct"
-    )
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
 
-    channel.queue_declare(
-        queue="router_jobs"
-    )
+    channel.queue_declare(queue="router_jobs")
 
     channel.queue_bind(
-        queue="router_jobs",
-        exchange="jobs",
-        routing_key="check_interfaces"
+        queue="router_jobs", exchange="jobs", routing_key="check_interfaces"
     )
 
-    channel.basic_publish(
-        exchange="jobs",
-        routing_key="check_interfaces",
-        body=body
-    )
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
 
     connection.close()
 
