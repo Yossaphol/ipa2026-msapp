bb0c05772d3b7e20ea3130bb1332a634d1ef7085
Yossaphol Manovorakul <yossaphol3502@gmail.com>
2026-09-15 10:09:47 +0700

    fix with black

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -16,27 +16,18 @@ def scheduler():
 
     while True:
         now = time.time()
-        now_str = time.strftime(
-            "%Y-%m-%d %H:%M:%S",
-            time.localtime(now)
-        )
+        now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
 
         ms = int((now % 1) * 1000)
         now_str_with_ms = f"{now_str}.{ms:03d}"
 
-        print(
-            f"[{now_str_with_ms}] run #{count}",
-            flush=True
-        )
+        print(f"[{now_str_with_ms}] run #{count}", flush=True)
 
         try:
             for data in get_router_info():
                 body_bytes = json_util.dumps(data).encode("utf-8")
 
-                produce(
-                    rabbitmq_host,
-                    body_bytes
-                )
+                produce(rabbitmq_host, body_bytes)
 
         except Exception as e:
             print(e, flush=True)
@@ -45,12 +36,7 @@ def scheduler():
         count += 1
         next_run += INTERVAL
 
-        time.sleep(
-            max(
-                0.0,
-                next_run - time.monotonic()
-            )
-        )
+        time.sleep(max(0.0, next_run - time.monotonic()))
 
 
 if __name__ == "__main__":
